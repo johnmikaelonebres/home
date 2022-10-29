cb02f3e6af2f174bdf5dc0551d0cac64c587365c
johnmikaelonebres <113188087+johnmikaelonebres@users.noreply.github.com>
2022-10-29 20:17:12 +0800

    Add files via upload

---
+++ javascript-platform/scriptbrowser.js
@@ -0,0 +1,25 @@
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'c2c1eda3a1msh3175c78dc587758p131463jsnc3f70434aaa9',
+    'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+  }
+};
+fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?platform=browser', options).then((data) => {
+  return data.json();
+}).then((completedata) => {
+  let data1 = "";
+  completedata.map((values) => {
+    data1 += `<div class="card">
+    <img src=${values.thumbnail} alt="img" class="images">
+    <h2 class="title">${values.title}</h2>
+    <h4 class="genre">Genre: ${values.genre}</h4>
+    <p class="short-desc">${values.short_description}</p>
+    <p class="platform">${values.platform}</p>
+    <a href="${values.game_url}" target="_blank">Play Now</a>
+    </div>`
+  });
+  document.getElementById("cards").innerHTML = data1
+}).catch((err) => {
+  console.log(err);
+})
